eb8d5876dd751a4450b503a4b6c99e1b017790f0
Tiffany Tam <tamtiffany89@gmail.com>
2022-03-14 10:58:21 -0400

    trying to deploy

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import react from "react";
+
 
 
 function About(){
@@ -8,19 +8,19 @@ function About(){
 
             <main id= "contributors">
                 <div className="sql _writers">DATA SCIENCE CONTRIBUTORS</div>
-                    <img src = "insert image url"/>
+                    <img src = "insert image url" alt = "louis profile"/>
                     <h3>LOUIS C</h3>
                     <p>Louis's biggest loves in life are superheroes and sql queries</p>
                 <div className="coders"> CONTRIBUTING DEVELOPERS</div>
-                    <img src = "insert image url"/>
+                    <img src = "insert image url" alt = "Tiffany profile"/>
                     <h3>Tiffany Tam</h3>
                     <p>Tiffany's biggest loves in life are her cats and coding</p>
                     
-                    <img src = "insert image url"/>                    
+                    <img src = "insert image url" alt = "Max profile"/>                    
                     <h3>Max Engel-Streich</h3>
                     <p>Max's biggest loves in life are brewing and coding</p>
                     
-                    <img src = "insert image url"/>
+                    <img src = "insert image url" alt = "Webster profile"/>
                     <h3>Webster Bian</h3>
                     <p>Webster's biggest loves in life are guns and coding</p>
             </main>
